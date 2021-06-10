6f152820cc90869b345b77f3a1fdf838f36308fa
aliyousafphularwan <victamoflove@gmail.com>
2021-06-10 18:07:29 +0500

    push

--- incs/home.php
+++ incs/home.php
@@ -23,7 +23,62 @@
 
     </div>
 
-    <div class="row">
+    <hr>
+
+    <div class="row mt-3">
+
+        <div class="col-md-6">
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-6"> Patients </div>
+                    <div class="col-md-6"> <p class="float-right"> view all </p> </div>
+                </div>
+
+                <div class="row">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th> Name </th>
+                                <th> Father </th>
+                                <th> Checkup date </th>
+                                <th> Details </th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-6">
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-6"> medicines </div>
+                    <div class="col-md-6"> <p class="float-right"> view all </p> </div>
+                </div>
+
+                <div class="row">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th> Name </th>
+                                <th> Formula </th>
+                                <th> Entry </th>
+                                <th> Details </th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
 
 </div>
